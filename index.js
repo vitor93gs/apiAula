e2777f746d8f26468a270a4dcd154b00e12c10a9
Vitor Santos <vitor93gs@gmail.com>
2022-02-23 09:45:20 -0300

    git inicial

---
+++ index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+const express = require("express");
+const app = express()
+app.use(express.json());
+app.listen(Number(process.env.PORT), () =>{
+    console.log(`Server up and running on port ${Number(process.env.PORT)}.`)
+})
